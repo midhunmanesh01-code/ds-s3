aba2302dec5294d5b0224006a174f8a9526b8436
Midhun Manesh <midhunmanesh01@gmail.com>
2026-07-16 14:13:48 +0530

    Add: Linear Search

---
+++ p2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+ int arr[100], n, i, key;
+ printf("Enter the number of elements: ");
+ scanf("%d", &n);
+ printf("Enter the elements:\n");
+ for(i = 0; i < n; i++) 
+ {
+  scanf("%d", &arr[i]);
+ }
+ printf("Enter the element to search: ");
+ scanf("%d", &key);
+ for(i = 0; i < n; i++) 
+ {
+  if(arr[i] == key) 
+  {
+   printf("Element found at position %d", i + 1);
+   return 0;
+  }
+ }
+ printf("Element not found");
+ return 0;
+}
