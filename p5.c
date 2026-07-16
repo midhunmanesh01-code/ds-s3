5dbe62402a127318872545a6789efa2670978e69
Midhun Manesh <midhunmanesh01@gmail.com>
2026-07-16 14:54:40 +0530

    Update Sparse Matrix Addition

--- p5.c
+++ p5.c
@@ -1,24 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-int a[20][3],b[20][3],c[20][3],i,n;
+int a[20][3], b[20][3], c[20][3];
+int i, n;
+
 printf("Enter number of non-zero elements: ");
-scanf("%d",&n);
+scanf("%d", &n);
+
 printf("Enter first sparse matrix (row col value):\n");
-for(i=0;i<n;i++)
- scanf("%d%d%d",&a[i][0],&a[i][1],&a[i][2]);
+for(i = 0; i < n; i++)
+scanf("%d %d %d", &a[i][0], &a[i][1], &a[i][2]);
 
 printf("Enter second sparse matrix (row col value):\n");
-for(i=0;i<n;i++)
- scanf("%d%d%d",&b[i][0],&b[i][1],&b[i][2]);
+for(i = 0; i < n; i++)
+scanf("%d %d %d", &b[i][0], &b[i][1], &b[i][2]);
 
 printf("\nResult:\n");
-for(i=0;i<n;i++)
+
+for(i = 0; i < n; i++)
 {
- c[i][0]=a[i][0];
- c[i][1]=a[i][1];
- c[i][2]=a[i][2]+b[i][2];
- printf("%d %d %d\n",c[i][0],c[i][1],c[i][2]);
+if(a[i][0] == b[i][0] && a[i][1] == b[i][1])
+{
+    c[i][0] = a[i][0];
+    c[i][1] = a[i][1];
+    c[i][2] = a[i][2] + b[i][2];
+
+    printf("%d %d %d\n", c[i][0], c[i][1], c[i][2]);
 }
+else
+{
+    printf("Addition not possible. Positions do not match.\n");
+    return 0;
+}
+}
+
 return 0;
 }
